ae5661b5eab3e1550248d97176d540f96c95b470
Tommy Hui <aesops0124@gmail.com>
2018-11-20 23:27:08 +0800

    Completed 2nd Test Case (4,2)

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,6 @@ const getMultiplication = require('../main');
 it ('should add get multiplication table', () => {
     expect(getMultiplication(2, 4)).toBe("2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12 4*4=16");
 });
+it ('should add get multiplication table', () => {
+    expect(getMultiplication(4, 2)).toBe(null);
+});
